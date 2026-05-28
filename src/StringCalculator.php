53e7a284c0f932b7c08a13aeb4080634ca93ee14
Lucas Bustince Labairu <lucasbustince07@gmail.com>
2026-05-28 11:10:19 +0200

    [verde] - Creado test en verde para poder elegir que separador usar

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -5,9 +5,15 @@ namespace Deg540\StringCalculatorPHP;
 class StringCalculator
 {
     public function add(string $numbers):int{
+        if(strlen($numbers) == 0){return 0;}
+        if(str_starts_with($numbers, "//")){
+            $separator = $numbers[2];
+            $numbers = substr($numbers, 4);
+            $nums = explode($separator, $numbers);
+            return array_sum($nums);
+        }
         $numbers = str_replace("\n", ",", $numbers);
         $nums = explode(",", $numbers);
-        if(strlen($numbers) == 0){return 0;}
         return array_sum($nums);
     }
 }
