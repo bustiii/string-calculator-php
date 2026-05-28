54482116a313a26f1f77e0575342398a9bf25327
Lucas Bustince Labairu <lucasbustince07@gmail.com>
2026-05-28 10:44:34 +0200

    [verde] - Creado test en verde para recibir una cadena de numeros con separacion de , o de \n y devolver la suma

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -5,6 +5,7 @@ namespace Deg540\StringCalculatorPHP;
 class StringCalculator
 {
     public function add(string $numbers):int{
+        $numbers = str_replace("\n", ",", $numbers);
         $nums = explode(",", $numbers);
         if(strlen($numbers) == 0){return 0;}
         return array_sum($nums);
