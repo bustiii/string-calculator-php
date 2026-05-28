ba95eb204d2f209d8afa046a9b418c764858b6b4
Lucas Bustince Labairu <lucasbustince07@gmail.com>
2026-05-28 10:30:17 +0200

    [verde] - Creado test en verde para sumar cadenas de mas de dos numeros

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -5,8 +5,9 @@ namespace Deg540\StringCalculatorPHP;
 class StringCalculator
 {
     public function add(string $numbers):int{
+        $partes = explode(",", $numbers);
         if(strlen($numbers) == 0){return 0;}
         if(strlen($numbers) == 1){return intval($numbers);}
-        return intval($numbers[0]) + intval($numbers[2]);
+        return array_sum($partes);
     }
 }
