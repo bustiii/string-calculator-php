36ea5a52dcf606352bda73245a1a66864b416b36
Lucas Bustince Labairu <lucasbustince07@gmail.com>
2026-05-28 11:25:00 +0200

    [verde] - Creado test en verde para no soportar numeros negativos

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,8 +4,9 @@ namespace Deg540\StringCalculatorPHP;
 
 class StringCalculator
 {
-    public function add(string $numbers):int{
+    public function add(string $numbers):int|string{
         if(strlen($numbers) == 0){return 0;}
+
         if(str_starts_with($numbers, "//")){
             $separator = $numbers[2];
             $numbers = substr($numbers, 4);
@@ -14,6 +15,12 @@ class StringCalculator
         }
         $numbers = str_replace("\n", ",", $numbers);
         $nums = explode(",", $numbers);
+
+        $negatives = array_filter($nums, fn($n) => $n < 0);
+        if(!empty($negatives)){
+            return "negativos no soportados: " . implode(",", $negatives);
+        }
+
         return array_sum($nums);
     }
 }
